b7c963db0305bd4439c9f00f1bb0cba95185cc0a
Irfan Yousuf <irfanusuf33@gmail.com>
2023-04-13 01:05:22 +0530

    v0.1.1

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
-// Importing Routes
-const post = require("./routes/post");
-const user = require("./routes/user");
 
 
-// Using Routes
-app.use("/api/v1", post);
-app.use("/api/v1", user);
+
 
 
 module.exports = app;
