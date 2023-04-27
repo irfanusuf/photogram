824ebecfd5fedf011a02a1e9c166a1d95b2d16b2
Irfan Yousuf <irfanusuf33@gmail.com>
2023-04-28 01:56:50 +0530

    addedMiddleWares

---
+++ backend/functionHandles/user.js
@@ -0,0 +1,47 @@
+const User = require("../models/User");
+const Post = require("../models/Post");
+const { sendEmail } = require("../middlewares/sendEmail");
+const crypto = require("crypto");
+const cloudinary = require("cloudinary");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, avatar } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+      folder: "avatars",
+    });
+
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar: { public_id: myCloud.public_id, url: myCloud.secure_url },
+    });
+
+    const token = await user.generateToken();
+
+    const options = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.status(201).cookie("token", token, options).json({
+      success: true,
+      user,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
